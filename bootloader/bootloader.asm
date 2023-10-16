25dd9d57af608269bab1a1d629a6aad88a3a445b
tom <tomhyhan@gmail.com>
2023-10-16 13:15:15 -0700

    bootloader

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,14 +1,30 @@
 org 0x7c00
 bits 16
-start: jmp boot
-
-msg db "Welcome to my os!", 0ah, 0dh, 0h
+jmp boot
 
 boot:
-  cli 
-  cld
-  hlt
+  mov  ax, 0003h    ; BIOS.SetVideoMode 80x25 16-color text
+  int  10h
 
+  mov  dx, 0C23h    ; DH is Row (12), DL is Column (35)
+  mov  bh, 0        ; DisplayPage
+  mov  ah, 02h      ; BIOS.SetCursorPosition
+  int  10h
+
+  mov  cx, 10       ; ReplicationCount
+  mov  bx, 002Fh    ; BH is DisplayPage (0) , BL is Attribute (BrightWhiteOnGreen)
+  mov  ax, 0941h    ; BIOS.WriteCharacterAndAttribute, AL is ASCII ("A")
+  int  10h
+
+  mov  ah, 00h      ; BIOS.WaitKeyboardKey
+  int  16h          ; -> AX
+    hlt
+
+MovCursor:
+    mov ah, 02h     ; Set cursor position BIOS function
+    int 0x10        ; Call BIOS interrupt
+    ret
+
+    
 times 510 - ($-$$) db 0
-dw 0xAA55
-
+dw 0xAA55 ; Boot Signature
