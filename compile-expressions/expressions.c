e83700322e0e154d45f6b5a183e04d01cb4b0fd6
tom <tomhyhan@gmail.com>
2024-02-05 12:38:21 -0800

    expression - assembly

---
+++ compile-expressions/expressions.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+int expr(int a, int b) {
+  int add = a + b;
+  return 0;
+}
+
+int main(void) {
+  int x = 3;
+  expr(3,4);
+  return 0;
+}
---
+++ compile-expressions/loop.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  int i = 0;
+  while (i < 3) {i++;}
+  return 0;
+}
