da98a43b8b18eaa810beaf8203f641b446cf81e9
tom <tomhyhan@gmail.com>
2024-02-12 12:44:30 -0800

    boot loader print welcome

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -8,30 +8,40 @@ bits 16
 start: jmp boot
 
 ; constant and variable definitions
- msg db "Welcome to My Operating System!", 0ah, 0dh, 0h
+msg db "Welcome to My Operating System!", 0ah, 0dh, 0h
 
 boot:
   cli ; no interrupts
   cld ; all that we need to init
 
-  mov ax, 0x50
+  mov ah, 0x02
+  mov bh, 0x00
+  mov dh, 15
+  mov dl, 10
+  int 0x10
+  mov si, msg
 
-  ;; set the buffer
-  mov es, ax
-  xor bx, bx
+  call put_char
 
-  mov al, 2 ; read 2 sectors
-  mov ch, 0 ; track 0
-  mov cl, 2 ; sector to read (2)
-  mov dh, 0 ; head number
-  mov dl, 0 ; drive number
-
-  mov ah, 0x02; read sectors from disk
-  int 0x13 ; call the buis routine
-  jmp 0x50:0x0 ; jump and execute the sectore
-  
   hlt ; halt the system
+put_char:
+    mov al, [si]
 
+    cmp al, 0
+    je END_PRINT
+
+    mov ah, 0x06
+    mov bh, 0x00
+    mov bl, 0x08
+    int 0x10
+
+    inc si
+
+    jmp put_char
+  
+END_PRINT:
+  ret
+  
 ; We have to be 512 bytes. Clear the rest of the bytes with 0
 times 510 - ($-$$) db 0
 dw 0xAA55 ; Boot Signiture
