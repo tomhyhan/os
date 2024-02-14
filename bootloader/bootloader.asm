a33ec15e55766ddff1885ba00e16ea9c9bff45a7
tom <tomhyhan@gmail.com>
2024-02-14 12:38:15 -0800

    bootloader

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -13,7 +13,25 @@ msg db "Welcome to My Operating System!", 0ah, 0dh, 0h
 boot:
   cli ; no interrupts
   cld ; all that we need to init
+  
+  mov ax, 0x50
 
+  mov es, ax
+  xor bx, bx
+
+  mov al, 2
+  mov ch, 0
+  mov cl, 2
+  mov dh, 0
+  mov dl, 0
+
+  mov ah, 0x02
+  int 0x13
+  jmp 0x50:0x0
+
+  hlt ; halt the system
+
+mov_cursor:
   mov ah, 0x02
   mov bh, 0x00
   mov dh, 15
@@ -21,16 +39,13 @@ boot:
   int 0x10
   mov si, msg
 
-  call put_char
-
-  hlt ; halt the system
 put_char:
     mov al, [si]
 
     cmp al, 0
     je END_PRINT
 
-    mov ah, 0x06
+    mov ah, 0x0e
     mov bh, 0x00
     mov bl, 0x08
     int 0x10
